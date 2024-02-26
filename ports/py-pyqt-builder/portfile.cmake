ae5822d5844e558a074d37e6cebce77d08dbf0b1
Matthias Kuhn <matthias@opengis.ch>
2024-02-26 22:53:01 +0100

    [py-pyqt-builder] Fix python lib path

--- ports/py-pyqt-builder/portfile.cmake
+++ ports/py-pyqt-builder/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_pythonhosted(
     PACKAGE_NAME    PyQt-builder
     VERSION         ${VERSION}
     SHA512          ec0b9f7784a32af744111615b93f98d73f284bb752fd71359c798d3b093a01925823effea72c866a5f49f77e3dfc5dee4125bbb289f647d84000bf34b5db6931
+    PATCHES
+      libpath.patch
 )
 
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -x)
