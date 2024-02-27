4589a548ae2769ce57ebbe0f516b1cdff120a885
Matthias Kuhn <matthias@opengis.ch>
2024-02-27 01:14:46 +0100

    temp

--- ports/py-pyqt6/portfile.cmake
+++ ports/py-pyqt6/portfile.cmake
@@ -1,4 +1,5 @@
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+set(VCPKG_BUILD_TYPE release)
 
 vcpkg_from_pythonhosted(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -6,6 +7,9 @@ vcpkg_from_pythonhosted(
     VERSION         ${VERSION}
     SHA512          c2ff8c47c9b0e43c009d0c90f565a54344e6f384c67dd30c2d422465d0702c07713acc0095c8b67827d1146675611c07d548ba282a26e41bb60a0a21977a7a64
 )
+
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
 
 set(SIPBUILD_ARGS
   "--confirm-license"
@@ -18,15 +22,19 @@ set(SIPBUILD_ARGS
   "--pep484-pyi"
   #  "--debug" # will create debuggable bindings
   "--target-dir" "${CURRENT_PACKAGES_DIR}/lib/python${PYTHON3_VERSION_MAJOR}.${PYTHON3_VERSION_MINOR}/site-packages"
-  "--build-dir" "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel-build"
+  "--build-dir" "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-sipbuild"
+  # "--qmake-setting" "'QMAKE_LIBS=\"${VCPKG_DETECTED_CMAKE_C_STANDARD_LIBRARIES} ${VCPKG_DETECTED_CMAKE_CXX_STANDARD_LIBRARIES}\"'"
+  "--qmake-setting" "'QMAKE_CFLAGS_RELEASE=\"${VCPKG_COMBINED_C_FLAGS_RELEASE}\"'"
+  "--qmake-setting" "'QMAKE_CXXFLAGS_RELEASE=\"${VCPKG_COMBINED_CXX_FLAGS_RELEASE}\"'"
+  "--qmake-setting" "'QMAKE_LFLAGS=\"${VCPKG_COMBINED_STATIC_LINKER_FLAGS_RELEASE}\"'"
+  "--qmake-setting" "'QMAKE_LFLAGS_SHLIB=\"${VCPKG_COMBINED_SHARED_LINKER_FLAGS_RELEASE}\"'"
 )
 
 set(path_backup "$ENV{PATH}")
 
 message(STATUS "Running sipbuild... (${PYTHON3})")
-
 # Make sure sipbuild can find QtCore.dll
-set(ENV{PATH} "${CURRENT_INSTALLED_DIR}/bin;$ENV{PATH}")
+set(ENV{PATH} "${CURRENT_INSTALLED_DIR}/lib;${CURRENT_INSTALLED_DIR}/tools/Qt6/bin/;${CURRENT_INSTALLED_DIR}/bin;$ENV{PATH}")
 
 vcpkg_execute_required_process(
         COMMAND "${PYTHON3}" "-m" "sipbuild.tools.build" ${SIPBUILD_ARGS}
@@ -34,6 +42,11 @@ vcpkg_execute_required_process(
         LOGNAME "sipbuild-${TARGET_TRIPLET}-0"
     )
 message(STATUS "Running sipbuild...finished.")
+
+vcpkg_configure_qmake(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-sipbuild")
+vcpkg_build_qmake() #TODO: just run jom
+return()
+
 
 
 if (CMAKE_HOST_WIN32)
