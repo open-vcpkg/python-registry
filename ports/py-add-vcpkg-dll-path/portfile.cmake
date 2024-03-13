5f78d4f5eccb40beea848b22d29853c873fa3679
Matthias Kuhn <matthias@opengis.ch>
2024-03-13 11:27:27 +0100

    Typo

--- ports/py-add-vcpkg-dll-path/portfile.cmake
+++ ports/py-add-vcpkg-dll-path/portfile.cmake
@@ -1,5 +1,5 @@
 file(INSTALL
-    "${CMAKE_CURRENT_LIST_DIR}sitecustomize.py"
+    "${CMAKE_CURRENT_LIST_DIR}/sitecustomize.py"
     DESTINATION "${CURRENT_PACKAGES_DIR}/${PYTHON3_SITE}")
 
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
