47d6b128c7ed7761e67d838c653d9bc88567beb7
Matthias Kuhn <matthias@opengis.ch>
2024-03-13 12:56:41 +0100

    Update sitecustomize.py

--- ports/py-add-vcpkg-dll-path/sitecustomize.py
+++ ports/py-add-vcpkg-dll-path/sitecustomize.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 vcpkg_bin_path = Path(sys.prefix) / '/../../bin'
 if not vcpkg_bin_path.is_dir():
-  raise RuntimeError('Could not add "{vcpkg_bin_path}" to dll paths because it does not exist.')
+  raise RuntimeError(f'Could not add "{vcpkg_bin_path}" to dll paths because it does not exist.')
 os.add_dll_directory(vcpkg_bin_path)
