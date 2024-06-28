115cf81d39042e40d13d646f5de34e4025ab7d8f
Matthias Kuhn <matthias@opengis.ch>
2024-06-28 15:36:56 +0200

    sha

--- ports/py-setuptools/portfile.cmake
+++ ports/py-setuptools/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_pythonhosted(
     OUT_SOURCE_PATH SOURCE_PATH
     PACKAGE_NAME    setuptools
     VERSION         ${VERSION}
-    SHA512          11df934931f4b73f7e07ea5713479593c6baa134d423556b2ae7aff0f1e5bdbdee1f5b516131adb169c838231ceb0293441fbf275ef7030dabecf74122565b6d
+    SHA512          63452aa1e1d1dea6ad622a21fcc9334bc3603e45de9a1485f79776e02a0fb3b0a0a8b3e886af73845c3c373348c7021f2e06a5665676e9015ee9855441512136
     PATCHES 
       fix-prefix.patch
 )
