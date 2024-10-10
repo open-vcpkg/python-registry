4dce128256a0f9a4911572507a098c0a5ba6aed9
Matthias Kuhn <matthias@opengis.ch>
2024-10-10 10:52:26 +0200

    No more cy river

--- ports/py-pyyaml/portfile.cmake
+++ ports/py-pyyaml/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_from_pythonhosted(
     VERSION         ${VERSION}
     SHA512          a675e0056bb6af9119c7f2796e349ced05afca64118a5b444d66dafcb6a774cf274eeba7eab044b86eb6c12a9d7c785c654f6cf229e1d190340dd06ea76e25ec
     FILENAME        pyyaml
-    PATCHES
-        cy-me-a-river.patch
 )
 
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
