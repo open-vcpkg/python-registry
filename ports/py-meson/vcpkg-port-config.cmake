4eb9197e9346d4b329d26176a4f9b5f127a1ddd8
Matthias Kuhn <matthias@opengis.ch>
2024-10-11 22:59:54 +0200

    Fix matplotlib

--- ports/py-meson/vcpkg-port-config.cmake
+++ ports/py-meson/vcpkg-port-config.cmake
@@ -1,3 +1,4 @@
 # Overwrite builtin scripts
 include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg-tool-meson/vcpkg-port-config.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg-get-python-packages/x_vcpkg_get_python_packages.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/vcpkg_mesonpy_prepare_build_options.cmake")
