69969a863bee8450a9a04cd2eef8fb2de12f6c89
Matthias Kuhn <matthias@opengis.ch>
2024-10-18 11:56:42 +0200

    add py-lxml, py-owslib and py-pytz

---
+++ ports/py-lxml/portfile.cmake
@@ -0,0 +1,13 @@
+vcpkg_from_pythonhosted(
+    OUT_SOURCE_PATH SOURCE_PATH
+    PACKAGE_NAME    lxml
+    VERSION         ${VERSION}
+    SHA512          7f8a3717645893bc6f790cc9adfb8fdab91c352dc4dc23c0ccb4af9a0d138acf9ef5054e9786af497955f10079e9242dbd63ea9ac39c33bfd71ca2fe4ef4a7c0
+)
+
+vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+vcpkg_python_test_import(MODULE "lxml")
+
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
