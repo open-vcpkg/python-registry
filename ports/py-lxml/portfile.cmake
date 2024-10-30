b7d41530e8c0d4a2e3af2bce35aacf3213e6e9a9
Nicolas Godet <39594821+nicogodet@users.noreply.github.com>
2024-10-30 12:20:15 +0100

    Fix py-lxml

--- ports/py-lxml/portfile.cmake
+++ ports/py-lxml/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_pythonhosted(
     SHA512          7f8a3717645893bc6f790cc9adfb8fdab91c352dc4dc23c0ccb4af9a0d138acf9ef5054e9786af497955f10079e9242dbd63ea9ac39c33bfd71ca2fe4ef4a7c0
 )
 
+set(ENV{INCLUDE} "${CURRENT_INSTALLED_DIR}/include;$ENV{INCLUDE}")
+
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
