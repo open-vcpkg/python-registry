1282b70859114a524b2584da98f78541986064bb
Matthias Kuhn <matthias@opengis.ch>
2024-11-03 22:34:38 +0100

    Fixes

--- ports/py-requests-kerberos/portfile.cmake
+++ ports/py-requests-kerberos/portfile.cmake
@@ -8,6 +8,6 @@ vcpkg_from_pythonhosted(
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-vcpkg_python_test_import(MODULE "requests_kerberos.HTTPKerberosAuth")
+vcpkg_python_test_import(MODULE "requests_kerberos")
 
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
