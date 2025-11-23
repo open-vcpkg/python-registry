d05917b7650d47b0583abd0aee85e2e771fbffdc
Matthias Kuhn <matthias@opengis.ch>
2025-11-23 21:33:12 +0100

    Add macos tests for lxml

--- ports/py-lxml/portfile.cmake
+++ ports/py-lxml/portfile.cmake
@@ -5,12 +5,14 @@ vcpkg_from_pythonhosted(
     SHA512          2aaa37a4504cd23d2022cbe7c2d99173aa0e3d6ec82a95e2429369ef3ef787db185440b8581db975cb81287c3ab533dd2e7cb447cefe0d7e628082f51c5b5254
 )
 
-file(WRITE "${SOURCE_PATH}/setup.cfg" "
+if(VCPKG_TARGET_IS_WINDOWS)
+  file(WRITE "${SOURCE_PATH}/setup.cfg" "
 [build_ext]
 include_dirs=${CURRENT_INSTALLED_DIR}/include;${CURRENT_INSTALLED_DIR}/include/libxml2
 library_dirs=${CURRENT_INSTALLED_DIR}/lib
 libraries=libxml2
 ")
+endif()
 
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
 
