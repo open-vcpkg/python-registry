f4c5722b46434a496d40c6c1aa883793758c62a5
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-03-12 11:39:14 +0100

    update DuckDb
    change of repo to duckdb/duckdb-python

--- ports/py-duckdb/portfile.cmake
+++ ports/py-duckdb/portfile.cmake
@@ -1,9 +1,12 @@
-vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO duckdb/duckdb
-        REF v${VERSION}
-        SHA512 d9b4cdc798212ddeb518d9c1d8a6640423ac05f9e8ce99855f96c63778a6757079da37fed17ad8ec131b3f28a9f89c3580d2bbacad03d7607d9d9150347f4903
-        HEAD_REF main
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_from_pythonhosted(
+    OUT_SOURCE_PATH SOURCE_PATH
+    PACKAGE_NAME    duckdb
+    VERSION         ${VERSION}
+    SHA512          42c0beeb9850379c524db9687f23da02f9c2205aaf23be1187cb0356a64f2b7cf8373f824b881c24db070e2f4b05986e0a0cc5c3c3a103b93019e3b30353cf15
 )
 
 z_vcpkg_make_prepare_flags()
@@ -13,9 +16,8 @@ foreach(buildtype IN LISTS buildtypes)
     z_vcpkg_make_prepare_env("${cmake_buildtype}" ${prepare_env_opts})
 endforeach()
 
-vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}/tools/pythonpkg")
+set(ENV{CMAKE_PREFIX_PATH} "${CURRENT_INSTALLED_DIR}")
+vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 vcpkg_python_test_import(MODULE "duckdb")
-
-set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
