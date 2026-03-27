4d75d43fd00ffc5a19189e37c314e85b30529594
Stuart C. Naifeh <stuart@cryptodira.org>
2026-03-27 13:47:01 -0400

    [arrow] bump version, fix python feature

--- ports/arrow/portfile.cmake
+++ ports/arrow/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_download_distfile(
     ARCHIVE_PATH
     URLS "https://archive.apache.org/dist/arrow/arrow-${VERSION}/apache-arrow-${VERSION}.tar.gz"
     FILENAME apache-arrow-${VERSION}.tar.gz
-    SHA512 8ec9ddaf7917c0e35c8bb32831fe6ea0a7b81de5723828a1289ba1b9e104b42af688d0f427a0ceff6f617d5f7ac67769431184b137e54f6987779e467c59d3ec
+    SHA512 c687e50dfcdbf7e0e39710224360d35d9aa734452b3a47adc8c101f3019b6b4116310c05b9f3cd0a5ed4ad9b7bd8fb88edb70e79b3cbd413a57e5e35e4554a6c
 )
 vcpkg_extract_source_archive(
     SOURCE_PATH
@@ -12,7 +12,8 @@ vcpkg_extract_source_archive(
         0003-android-musl.patch
         0004-android-datetime.patch
         0005-cmake-msvcruntime.patch
-        0006-pcg-msvc-arm64.patch
+        0007-use-vcpkg-mimalloc.patch
+        python-fix-cli-params.patch
 )
 
 # Check cpp/cmake_modules/DefineOptions.cmake for option dependencies -
@@ -166,6 +167,7 @@ if("python" IN_LIST FEATURES)
         OUT_PYTHON_VAR PYTHON3_VENV
     )
 
+    message(STATUS "Building and installing extension")
     set(ENV{Arrow_DIR} "${CURRENT_PACKAGES_DIR}/share/arrow")
     set(ENV{ArrowCompute_DIR} "${CURRENT_PACKAGES_DIR}/share/arrowcompute")
     if("dataset" IN_LIST FEATURES)
@@ -187,27 +189,17 @@ if("python" IN_LIST FEATURES)
     set(ENV{SETUPTOOLS_SCM_PRETEND_VERSION} "${VERSION}")
     set(ENV{PDM_BUILD_SCM_VERSION} "${VERSION}")
 
-    set(install_prefix "${CURRENT_INSTALLED_DIR}")
-    if(VCPKG_TARGET_IS_WINDOWS)
-        string(APPEND install_prefix "/tools/python3")
-    endif()
-
     if (NOT "${VCPKG_BUILD_TYPE}" STREQUAL "")
         set(build_opts "--build-type=${VCPKG_BUILD_TYPE}")
     else()
         set(build_opts "--build-type=release")
     endif()
+    
     vcpkg_execute_required_process(
         COMMAND "${PYTHON3_VENV}" "setup.py"  
-        "build_ext" "${build_opts}" "--bundle-arrow-cpp"
-        "install" "--prefix" "${install_prefix}"
+        "build_ext" "${build_opts}" --rpath "@loader_path/../../../"
+        "install"  "--prefix" "${CURRENT_PACKAGES_DIR}" 
         LOGNAME "python-build-${TARGET_TRIPLET}"
-        WORKING_DIRECTORY "${SOURCE_PATH}/python"
-    )
-
-    message(STATUS "Testing pyarrow")
-    vcpkg_execute_required_process(COMMAND "${PYTHON3}" "${SOURCE_PATH}/python/scripts/test_imports.py"
-        LOGNAME "python-import-test-${TARGET_TRIPLET}"
         WORKING_DIRECTORY "${SOURCE_PATH}/python"
     )
 endif()
