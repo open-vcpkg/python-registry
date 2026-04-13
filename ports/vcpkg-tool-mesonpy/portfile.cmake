42a0ff4f6c571e0c8d318f7e6e37566309aeeb3c
Stuart C. Naifeh <stuart@cryptodira.org>
2026-04-13 09:53:51 -0400

    move mesonpy vcpkg scripts to separate port

---
+++ ports/vcpkg-tool-mesonpy/portfile.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_POLICY_CMAKE_HELPER_PORT enabled)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-port-config.cmake" @ONLY)
+
+file(COPY 
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg_mesonpy_prepare_build_options.cmake"
+    DESTINATION 
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
