ea4ed21e7f0d730899282c0c2384bbe40c995c37
Matthias Kuhn <matthias@opengis.ch>
2026-05-22 20:48:04 +0200

    configure via env

--- ports/py-psycopg-c/portfile.cmake
+++ ports/py-psycopg-c/portfile.cmake
@@ -4,17 +4,12 @@ vcpkg_from_pythonhosted(
     VERSION         ${VERSION}
     SHA512          ab3299f090dc594a2036398fee10a9b63548b7de62dbcbd4041b806a105212b62a7aff7b7a6003a1bf041bc4846ea8a9e2c25e432234059f26c55e603190d608
     FILENAME        psycopg_c
+    PATCHES
+      env_configurable.patch
 )
 
-if(VCPKG_TARGET_IS_WINDOWS)
-  vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/tools/libpq")
-  set(ENV{PG_CONFIG} "${CURRENT_INSTALLED_DIR}/tools/libpq/pg_config.exe")
-else()
-  vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/tools/libpq")
-  set(ENV{PG_CONFIG} "${CURRENT_INSTALLED_DIR}/tools/libpq/pg_config")
-endif()
-
-set(ENV{INCLUDE} "${CURRENT_INSTALLED_DIR}/include;$ENV{INCLUDE}")
+set(ENV{PSYCOPG_PG_INCLUDEDIR} "${CURRENT_INSTALLED_DIR}/include")
+set(ENV{PSYCOPG_PG_LIBDIR} "${CURRENT_INSTALLED_DIR}/lib")
 
 vcpkg_python_build_and_install_wheel(
   SOURCE_PATH "${SOURCE_PATH}"
