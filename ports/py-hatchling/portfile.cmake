4ed793f510373dbd364486afe8c5c6e79fbdaf68
Matthias Kuhn <matthias@opengis.ch>
2026-08-11 15:17:20 +0200

    Autoupdating ports (#266)
    Co-authored-by: Update Bot <updatebot@open-vcpkg>

--- ports/py-hatchling/portfile.cmake
+++ ports/py-hatchling/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_pythonhosted(
     OUT_SOURCE_PATH SOURCE_PATH
     PACKAGE_NAME    hatchling
     VERSION         ${VERSION}
-    SHA512          e48055970b0b6d0752d704c89c660d11530c2594c24eb8d4660b6ae53d79fa8170b0306481c8a8bca3d06feb4476bcdd79d9c1a5dea3c7a4f84a762ecfd5c594
+    SHA512          8a4989943705f547f848ca6d46e33cf3b4949c81b21dff38baf48b532b6ea415cd0558538a8f404a1fd09d8d97ec47c36cb7b1e350aef9b8840f55380841de34
 )
 
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
