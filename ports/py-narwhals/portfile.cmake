92fc9d9c0a928a55654f11e21645a720920c5d9f
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-08-31 15:44:46 +0200

    remove patch

--- ports/py-narwhals/portfile.cmake
+++ ports/py-narwhals/portfile.cmake
@@ -5,8 +5,14 @@ vcpkg_from_pythonhosted(
     PACKAGE_NAME    narwhals
     VERSION         ${VERSION}
     SHA512          225190b5e31f3fd5ac514527bd8dcb3c50ae7f4abd3620ae1cf5a28b5f88b5c115b590705eb3c63de97dd8c5e172fd892286c3f896439815a3a83c076c18b3ba
-    PATCHES
-        use-hatchling.patch
+)
+
+# uv_build is not available, build with hatchling instead
+# (matched by regex so that upstream bumping the uv_build version range does not break this)
+vcpkg_replace_string("${SOURCE_PATH}/pyproject.toml"
+    "requires = \\[\"uv_build[^\n]*\nbuild-backend = \"uv_build\""
+    "requires = [\"hatchling\"]\nbuild-backend = \"hatchling.build\"\n\n[tool.hatch.build.targets.wheel]\npackages = [\"src/narwhals\"]"
+    REGEX
 )
 
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
