a4cb7d431c3558fcdc801ffb5056e84ea1ac8cec
Matthias Kuhn <matthias@opengis.ch>
2026-09-01 13:40:50 +0200

    Autoupdating ports (#277)
    Co-authored-by: Update Bot <updatebot@open-vcpkg>

--- ports/py-fonttools/portfile.cmake
+++ ports/py-fonttools/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_pythonhosted(
     OUT_SOURCE_PATH SOURCE_PATH
     PACKAGE_NAME    fonttools
     VERSION         ${VERSION}
-    SHA512          e2bde6283aa2c88b0cef56557d1ef7cebbdf14abfdbde2a1cea6b4aba79a6d920c8eef2844d57b4d9db0b2647fa130f5f08cae62a71be9c9cbc2c95e6e52c081
+    SHA512          9414fd480b81cf7ad4b963ada3389011dff1549d9ae888b02d293769a18a3d7fcb7c99f6856038a13aa6057bc6898e096958dd7cb475b38007ce05a041957516
 )
 
 vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}")
